28ae6fee5a3d986eac35a5738f2613c557935400
JonnyLatte <johnlaloli@gmail.com>
2017-01-27 01:24:44 +1100

    Update stFundManager.sol

--- stFundManager.sol
+++ stFundManager.sol
@@ -1,29 +1,7 @@
 pragma solidity ^0.4.4;
 
 import "github.com/JonnyLatte/MiscSolidity/multiToken.sol";
-
-contract verySig {
-    
-    function checkSig(bytes32 hash, bytes sig) internal returns(address) 
-    {
-        //https://gitter.im/ethereum/solidity/archives/2016/12/14
-        
-        uint8   v;
-        bytes32 r;
-        bytes32 s;
-
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := and(mload(add(sig, 65)), 255)
-        }
-    
-        if (v < 27) v += 27;
-    
-        return ecrecover(hash, v, r, s) ;
-    }    
-}
-
+import "github.com/JonnyLatte/MiscSolidity/verySig.sol";
 
 contract stFundManager is fundManager, verySig {
     
