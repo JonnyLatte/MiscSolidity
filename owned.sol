792466eff91acae779a87ef24a2d68fc7b2e0acb
JonnyLatte <johnlaloli@gmail.com>
2017-01-27 04:06:23 +1100

    Update owned.sol

--- owned.sol
+++ owned.sol
@@ -17,3 +17,35 @@ contract owned {
     }
 }
 
+contract ownedWithAproval
+{
+    address public approvedOwner;
+    address public owner;
+
+    function owned() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender != owner) throw;
+        _;
+    }
+
+    function transferOwnership(address newOwner) 
+        onlyOwner 
+        returns (bool ok)
+    {
+        owner = newOwner;
+        approvedOwner = newOwner;
+    }
+    
+    function approve(address newPotentialOwner) {
+        approvedOwner = newPotentialOwner;
+    }
+    
+    function claimOwnership() returns (bool ok) {
+        if(msg.sender != approvedOwner) throw;
+        owner = approvedOwner;
+        return true;
+    }
+}
