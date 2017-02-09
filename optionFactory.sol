d146a48f7aceec773078572c70f9eadb95b8e1bf
JonnyLatte <johnlaloli@gmail.com>
2017-02-10 08:07:21 +1100

    Update optionFactory.sol

--- optionFactory.sol
+++ optionFactory.sol
@@ -1,62 +1,145 @@
 pragma solidity ^0.4.4;
 
-import "github.com/JonnyLatte/MiscSolidity/optionToken.sol"; 
+import "github.com/JonnyLatte/MiscSolidity/ownedToken.sol";
 
-// optionFactory: create single use option tokens
-//
-// TODO: comments, events
-
-contract optionFactory {
-    
-    uint256 last_option_id;
-    mapping(uint256 => optionToken) public options;
-    mapping(address => bool) _valid;
-    
-    function next_option_id() internal returns (uint) {
-        last_option_id++;
-        return last_option_id;
+contract optionFactory 
+{
+    struct OPTION_INFO 
+    {
+        address    owner;
+        ERC20      sell_token;
+        uint       sell_units;
+        ERC20      buy_token;
+        uint       buy_units;
+        uint       expiry;
+        ownedToken token;
     }
     
-    function createOptionToken(
-        address _currency, 
-        address _asset, 
-        uint256 _price, 
-        uint256 _units, 
-        uint256 _duration) returns (optionToken) 
+    uint public last_option_id;
+    mapping(uint => OPTION_INFO) option_info;
+
+    //--------------------------------------------------------------------------------
+
+    event mintEvent(uint id, uint unitLots);
+    event burnEvent(uint id, uint unitLots);
+    event exerciseEvent(uint id, uint unitLots);
+    event closeEvent(uint id);
+    
+    event optionCreatedEvent(
+        uint id,
+        address owner,
+        ERC20 sell_token,
+        uint  sell_units,
+        ERC20 buy_token,
+        uint  buy_units,
+        uint expiry,
+        ERC20 token);
+
+    //--------------------------------------------------------------------------------
+
+    function next_option_id() internal returns (uint) 
     {
-        var token = new optionToken(
-         _currency, 
-         _asset, 
-         _price, 
-         _units, 
-         _duration);     
-         
-        if(!token.transferOwnership(msg.sender)) throw;
+        return ++last_option_id;
+    }    
+    
+    function create_option(        
+        ERC20 sell_token,
+        uint  sell_units,
+        ERC20 buy_token,
+        uint  buy_units,
+        uint time_to_expiry) returns (uint id, ownedToken token)
+    {
+        id = next_option_id();
+        token = new ownedToken();
         
-        var id = next_option_id();
-        options[id] = token;
-        _valid[token] = true;
+        if(address(token) == 0) throw;
         
-        return token;
+        var expiry = now + time_to_expiry;
+        
+        option_info[id] = OPTION_INFO (
+            msg.sender,
+            sell_token,
+            sell_units,
+            buy_token,
+            buy_units,
+            expiry,
+            token);
+            
+        optionCreatedEvent(id,msg.sender,sell_token, sell_units,buy_token,buy_units,expiry,token);
     }
     
-    function verify(optionToken addr) constant returns 
-    (
-        bool    valid,
-        address option_owner,
-        address option_currency, 
-        address option_asset, 
-        uint256 option_price, 
-        uint256 option_units, 
-        uint256 option_expireTime)
+    function optionInfo(uint id) constant returns(
+        address owner,
+        ERC20 sell_token,
+        uint  sell_units,
+        ERC20 buy_token,
+        uint  buy_units,
+        uint  expiry,
+        uint  time_to_expiry,
+        address token) 
     {
-        valid  = _valid[addr];
+        var option = option_info[id];
         
-        option_owner      = optionToken(addr).owner();
-        option_currency   = optionToken(addr).currency();
-        option_asset      = optionToken(addr).asset();
-        option_price      = optionToken(addr).price();
-        option_units      = optionToken(addr).units();
-        option_expireTime = optionToken(addr).expireTime();
+        owner      = option.owner;
+        sell_token = option.sell_token;
+        sell_units = option.sell_units;
+        buy_token  = option.buy_token;
+        buy_units  = option.buy_units;
+        expiry     = option.expiry;
+        
+        if(expiry > now) time_to_expiry = expiry - now;
+        
+        token = option.token;
+    }
+    
+    function mint(uint id, uint unitLots) 
+    {
+        var option = option_info[id];
+        
+        if(option.owner != msg.sender) throw;
+        
+        if(!option.sell_token.transferFrom(msg.sender,this,unitLots*option.sell_units)) throw;
+        option.token.issue(unitLots,msg.sender);
+        
+        mintEvent(id,unitLots);
+    }
+    
+    function burn(uint id, uint unitLots) 
+    {
+        var option = option_info[id];
+        
+        if(option.owner != msg.sender) throw;
+        
+        if(!option.token.burn(unitLots,msg.sender)) throw;
+        if(!option.sell_token.transfer(msg.sender,unitLots*option.sell_units)) throw;
+        
+        burnEvent(id,unitLots);
+    }
+    
+    function exercise(uint id, uint unitLots) 
+    {
+        var option = option_info[id];
+        
+        if(option.expiry > now) throw;
+        
+        if(!option.token.burn(unitLots,msg.sender)) throw;
+        if(!option.buy_token.transferFrom (msg.sender,option.owner,unitLots*option.buy_units )) throw;
+        if(!option.sell_token.transferFrom(this      ,msg.sender  ,unitLots*option.sell_units)) throw;
+        
+        exerciseEvent(id,unitLots);
+    }
+    
+    function close(uint id) 
+    {
+        var option = option_info[id];
+        
+        if(option.owner != msg.sender) throw;   
+        
+        var supply = option.token.totalSupply();
+        if(!option.sell_token.transfer(msg.sender,supply)) throw;
+        option.token.destroy();
+        delete option_info[id];
+        
+        closeEvent(id);
     }
 }
