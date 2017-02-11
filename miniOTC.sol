e270fdf299b41c2c14b93b6f6fc51dcdc1b89c32
JonnyLatte <johnlaloli@gmail.com>
2017-02-11 19:56:20 +1100

    Update miniOTC.sol

--- miniOTC.sol
+++ miniOTC.sol
@@ -33,7 +33,7 @@ contract miniOTCFactory {
     
     event tradeListing(address contractAddress, Token indexed _maker_buy_token, uint256 _maker_buy_units, Token indexed _maker_sell_token, uint256 _maker_sell_units);
     
-    function  createTradeMicro (Token _maker_buy_token, uint256 _maker_buy_units, Token _maker_sell_token, uint256 _maker_sell_units) 
+    function  createTrade(Token _maker_buy_token, uint256 _maker_buy_units, Token _maker_sell_token, uint256 _maker_sell_units) 
     {
         tradeListing(
             new miniOTC(_maker_buy_token, _maker_buy_units, _maker_sell_token, _maker_sell_units) , 
