06deb2ea5507d6c59553946bdaecee38c20698a5
JonnyLatte <johnlaloli@gmail.com>
2017-03-05 01:19:44 +1100

    swicth to safeMul for overflow checking

--- SafeMath.sol
+++ SafeMath.sol
@@ -1,26 +1,55 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.8;
 
-contract SafeMath {
-  //internals
+contract Token {
+    function transferFrom( address from, address to, uint value) returns (bool ok);
+}
 
-  function safeMul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+contract miniOTC {
+    
+    Token   public maker_buy_token;
+    uint256 public maker_buy_units;
+    Token   public maker_sell_token;
+    uint256 public maker_sell_units;
+    
+    event onTrade(uint256 unitLots);
+    
+    function  miniOTC(Token _maker_buy_token, uint256 _maker_buy_units, Token _maker_sell_token, uint256 _maker_sell_units) 
+    {
+       maker_buy_token   = _maker_buy_token;
+       maker_buy_units   = _maker_buy_units; 
+       maker_sell_token  = _maker_sell_token; 
+       maker_sell_units  = _maker_sell_units; 
+    }
+    
+    function assert(bool assertion) internal {
+       if (!assertion) throw;  
+    }
 
-  function safeSub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
+    function safeMul(uint a, uint b) internal returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+    
+    function trade(address maker, uint256 unitLots) 
+    {
+        if(!maker_buy_token.transferFrom(msg.sender,maker,safeMul(unitLots, maker_buy_units))) throw;
+        if(!maker_sell_token.transferFrom(maker,msg.sender,safeMul(unitLots, maker_sell_units))) throw;
+        onTrade(unitLots);
+    }
+}
 
-  function safeAdd(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
-
-  function assert(bool assertion) internal {
-    if (!assertion) throw;
-  }
+contract miniOTCFactory {
+    
+    event tradeListing(address contractAddress, Token indexed _maker_buy_token, uint256 _maker_buy_units, Token indexed _maker_sell_token, uint256 _maker_sell_units);
+    
+    function  createTrade(Token _maker_buy_token, uint256 _maker_buy_units, Token _maker_sell_token, uint256 _maker_sell_units) 
+    {
+        tradeListing(
+            new miniOTC(_maker_buy_token, _maker_buy_units, _maker_sell_token, _maker_sell_units) , 
+            _maker_buy_token, 
+            _maker_buy_units, 
+            _maker_sell_token, 
+            _maker_sell_units);       
+    }
 }
