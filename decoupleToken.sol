5fa3bb214d27b8905b8d3b5069cd649b4285fe95
JonnyLatte <johnlaloli@gmail.com>
2017-04-29 19:40:31 +1000

    Create decoupleToken.sol

---
+++ decoupleToken.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.4;
+
+import "github.com/JonnyLatte/MiscSolidity/baseToken.sol"; 
+import "github.com/JonnyLatte/MiscSolidity/verySig.sol"; 
+
+// base token that allows anyone to submit a transaction on behalf of an addres in return for a fee
+// TODO: decoupled approval and transferFrom
+
+contract decoupleToken is baseToken , verySig {
+    
+    mapping(address => mapping(uint => bool)) public usedNonce;
+    
+    function appTransfer(address from, address to, uint value) internal 
+    {
+        _balances[from]  = safeSub(_balances[from], value);              // will throw if inssufficient funds
+        _balances[to]    = safeAdd(_balances[to], value);                // will throw if overflow
+    }
+    
+    function getTranferHash(address from, address to, uint value, uint fee, uint nonce) constant returns(bytes32) {
+        return sha3(this,from,to,value,fee,nonce);
+    }
+    
+    function decoupledTransfer(address from, address to, uint value, uint fee, uint nonce, bytes sig) {
+        
+        var hash = getTranferHash(from, to, value, fee, nonce);
+        if(from != checkSig(hash, sig)) throw;
+        
+        if(usedNonce[from][nonce] == false)
+        {
+           usedNonce[from][nonce] = true;
+           
+           if(_balances[from] < fee) return; // return if cant pay fee
+           appTransfer(from, msg.sender, fee); 
+           
+           if(_balances[from] < value) return; // return if cant pay
+           appTransfer(from, to, value);       
+        }
+    }
+}
