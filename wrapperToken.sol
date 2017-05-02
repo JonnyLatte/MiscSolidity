970ee055b6a1bd7c2116f1a01851d574e0d0052e
JonnyLatte <johnlaloli@gmail.com>
2017-05-02 22:02:48 +1000

    Update wrapperToken.sol
    update to SafeMath

--- wrapperToken.sol
+++ wrapperToken.sol
@@ -1,20 +1,19 @@
-pragma solidity ^0.4.4;
+
+pragma solidity ^0.4.8;
 
 import "github.com/JonnyLatte/MiscSolidity/baseToken.sol";
 
 contract wrapperToken is baseToken {
     
     function deposit() payable returns (bool ok) {
-        if (_balances[msg.sender] + msg.value < _balances[msg.sender]) throw;
-        _balances[msg.sender] += msg.value;
-        _supply += msg.value;
+        _balances[msg.sender] = safeAdd(_balances[msg.sender],msg.value);
+        _supply = safeAdd(_supply,msg.value);
         return true;
     }
     
     function withdraw(uint256 value) returns (bool ok) {
-        if (_balances[msg.sender] < value) throw;
-        _balances[msg.sender] -= value;
-        _supply -= value;
+        _balances[msg.sender] = safeSub(_balances[msg.sender],value);
+        _supply = safeSub(_supply,value);
         
         if(!msg.sender.send(value)) throw;
         
@@ -22,10 +21,6 @@ contract wrapperToken is baseToken {
     }
     
     function() payable { 
-        if (_balances[msg.sender] + msg.value < _balances[msg.sender]) throw;
-        _balances[msg.sender] += msg.value;
-        _supply += msg.value;
+        deposit();
     }
-
-    
 }
