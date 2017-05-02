c2261d5b4038db13db44acc62f34612ebacab32c
JonnyLatte <johnlaloli@gmail.com>
2017-05-02 21:37:51 +1000

    Update ownedToken.sol
    update to safe math

--- ownedToken.sol
+++ ownedToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.8;
 
 import "github.com/JonnyLatte/MiscSolidity/baseToken.sol";
 import "github.com/JonnyLatte/MiscSolidity/owned.sol";
@@ -7,17 +7,15 @@ contract ownedToken is baseToken, owned  {
     
     function issue(uint256 value, address to) onlyOwner  returns (bool ok)
     {
-        if (_balances[to] + value < _balances[to]) throw; // Check for overflows
-        _balances[to] += value;
-        _supply += value;
+        _balances[to] = safeAdd(_balances[to],value);
+        _supply = safeAdd(_supply,value);
         return true;
     }
     
     function burn(uint256 value, address from) onlyOwner  returns (bool ok)
     {
-        if (_balances[from] < value) throw;
-        _balances[from] -= value;
-        _supply -= value;
+        _balances[from] = safeSub(_balances[from],value);
+        _supply = safeSub(_supply,value);
         return true;
     }
     
