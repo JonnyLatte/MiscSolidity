19f8cdde683229586aa51238dc156d9e2e2b7543
JonnyLatte <johnlaloli@gmail.com>
2017-05-11 22:39:17 +1000

    Update parseInt.sol

--- parseInt.sol
+++ parseInt.sol
@@ -21,7 +21,7 @@ pragma solidity ^0.4.8;
     
 contract parseInt {
      
-    function strToInt( string str, uint index, uint base) returns (uint,uint) {
+    function strToInt( string str, uint index, uint base) constant returns (uint,uint) {
         
         uint sum = 0;
         uint i   = 0;
@@ -32,10 +32,11 @@ contract parseInt {
             uint c = (uint)(bytes(str)[i]);
             if(c < 48) break;
             sum = sum * base;
-            if(bytes(str)[i] < 58) sum +=  c- 48;
-            else sum += c - 87;
+            if(c <= 57) sum += c - 48; // '0'..'9'
+            else if(c < 97) sum +=  c - 55; // 'a'..'z'
+            else sum += c - 87; // 'A'..'Z'
         }
         
         return (sum,i+1);
-    }   
+    }      
 }
