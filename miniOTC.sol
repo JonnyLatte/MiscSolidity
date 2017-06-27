4e63c409a3fee266a08b0697c9f993ea49757347
JonnyLatte <johnlaloli@gmail.com>
2017-06-28 04:17:41 +1000

    Update miniOTC.sol

--- miniOTC.sol
+++ miniOTC.sol
@@ -1,49 +1,101 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.10;
 
-contract Token {
+contract ERC20 {
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval( address indexed owner, address indexed spender, uint value);
+    function totalSupply() constant returns (uint supply);
+    function balanceOf( address who ) constant returns (uint value);
+    function allowance(address owner, address spender) constant returns (uint _allowance);   
+    function transfer( address to, uint value) returns (bool ok);
     function transferFrom( address from, address to, uint value) returns (bool ok);
+    function approve(address spender, uint value) returns (bool ok);
 }
+
+
+library SafeMath 
+{
+  function safeMul(uint a, uint b) internal returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function safeDiv(uint a, uint b) internal returns (uint) {
+    assert(b > 0);
+    uint c = a / b;
+    assert(a == b * c + a % b);
+    return c;
+  }
+
+  function safeSub(uint a, uint b) internal returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function safeAdd(uint a, uint b) internal returns (uint) {
+    uint c = a + b;
+    assert(c>=a && c>=b);
+    return c;
+  }
+
+  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
+    return a < b ? a : b;
+  }
+
+  function assert(bool assertion) internal {
+    if (!assertion) {
+      throw;
+    }
+  }
+}
+
 
 contract miniOTC {
     
-    Token   public maker_buy_token;
-    uint256 public maker_buy_units;
-    Token   public maker_sell_token;
-    uint256 public maker_sell_units;
+    using SafeMath for uint;
     
+    ERC20   public maker_buy_token;
+    uint256 public maker_buy_units;
+    ERC20   public maker_sell_token;
+    uint256 public maker_sell_units;
+
     event onTrade(uint256 unitLots);
     
-    function  miniOTC(Token _maker_buy_token, uint256 _maker_buy_units, Token _maker_sell_token, uint256 _maker_sell_units) 
+    function  miniOTC(ERC20 _maker_buy_token, uint256 _maker_buy_units, ERC20 _maker_sell_token, uint256 _maker_sell_units) 
     {
-       maker_buy_token   = _maker_buy_token;
-       maker_buy_units   = _maker_buy_units; 
-       maker_sell_token  = _maker_sell_token; 
-       maker_sell_units  = _maker_sell_units; 
-    }
-    
-    function assert(bool assertion) internal {
-       if (!assertion) throw;  
+       maker_buy_token    = _maker_buy_token;
+       maker_buy_units    = _maker_buy_units; 
+       maker_sell_token   = _maker_sell_token; 
+       maker_sell_units   = _maker_sell_units; 
     }
 
-    function safeMul(uint a, uint b) internal returns (uint) {
-        uint c = a * b;
-        assert(a == 0 || c / a == b);
-        return c;
-    }
-    
-    function trade(address maker, uint256 unitLots) 
+    function trade(address taker, address maker, uint256 unitLots)
     {
-        if(!maker_buy_token.transferFrom(msg.sender,maker,safeMul(unitLots, maker_buy_units))) throw;
-        if(!maker_sell_token.transferFrom(maker,msg.sender,safeMul(unitLots, maker_sell_units))) throw;
+        uint takerValue = unitLots.safeMul(maker_buy_units);
+        uint makerValue = unitLots.safeMul(maker_sell_units);
+        require(maker_buy_token.transferFrom(taker,maker,takerValue)); // pay maker
+        require(maker_sell_token.transferFrom(maker,taker,makerValue));// pay taker
         onTrade(unitLots);
     }
 }
 
 contract miniOTCFactory {
     
-    event tradeListing(address contractAddress, Token indexed _maker_buy_token, uint256 _maker_buy_units, Token indexed _maker_sell_token, uint256 _maker_sell_units);
+    event tradeListing(address contractAddress, ERC20 indexed _maker_buy_token, uint256 _maker_buy_units, ERC20 indexed _maker_sell_token, uint256 _maker_sell_units);
     
-    function  createTrade(Token _maker_buy_token, uint256 _maker_buy_units, Token _maker_sell_token, uint256 _maker_sell_units) 
+    function  createTrade(ERC20 _maker_buy_token, uint256 _maker_buy_units, ERC20 _maker_sell_token, uint256 _maker_sell_units) 
     {
         tradeListing(
             new miniOTC(_maker_buy_token, _maker_buy_units, _maker_sell_token, _maker_sell_units) , 
@@ -53,5 +105,3 @@ contract miniOTCFactory {
             _maker_sell_units);       
     }
 }
-
-
