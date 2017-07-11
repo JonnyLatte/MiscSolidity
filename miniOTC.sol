ce5a57abab1655a54a62d41f7c132db1df8362c8
JonnyLatte <johnlaloli@gmail.com>
2017-07-11 10:16:11 +1000

    SafeMath changes

--- miniOTC.sol
+++ miniOTC.sol
@@ -1,66 +1,7 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.12;
 
-contract ERC20 {
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval( address indexed owner, address indexed spender, uint value);
-    function totalSupply() constant returns (uint supply);
-    function balanceOf( address who ) constant returns (uint value);
-    function allowance(address owner, address spender) constant returns (uint _allowance);   
-    function transfer( address to, uint value) returns (bool ok);
-    function transferFrom( address from, address to, uint value) returns (bool ok);
-    function approve(address spender, uint value) returns (bool ok);
-}
-
-
-library SafeMath 
-{
-  function safeMul(uint a, uint b) internal returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
-
-  function safeDiv(uint a, uint b) internal returns (uint) {
-    assert(b > 0);
-    uint c = a / b;
-    assert(a == b * c + a % b);
-    return c;
-  }
-
-  function safeSub(uint a, uint b) internal returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  function safeAdd(uint a, uint b) internal returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
-
-  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a >= b ? a : b;
-  }
-
-  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a < b ? a : b;
-  }
-
-  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a >= b ? a : b;
-  }
-
-  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a < b ? a : b;
-  }
-
-  function assert(bool assertion) internal {
-    if (!assertion) {
-      throw;
-    }
-  }
-}
-
+import "github.com/JonnyLatte/MiscSolidity/SafeMath.sol";
+import "github.com/JonnyLatte/MiscSolidity/erc20.sol";
 
 contract miniOTC {
     
