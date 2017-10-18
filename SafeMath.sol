6c2a6a124afbc4d31df7a7861d2346557c0e3429
JonnyLatte <johnlaloli@gmail.com>
2017-10-19 01:26:44 +1100

    Update SafeMath.sol

--- SafeMath.sol
+++ SafeMath.sol
@@ -2,35 +2,35 @@ pragma solidity ^0.4.12;
 
 library SafeMath 
 {
-  function safeMul(uint a, uint b) internal returns (uint) {
+  function safeMul(uint a, uint b) internal pure returns (uint) {
     uint c = a * b;
     assert(a == 0 || c / a == b);
     return c;
   }
 
-  function safeDiv(uint a, uint b) internal returns (uint) {
+  function safeDiv(uint a, uint b) internal pure returns (uint) {
     assert(b > 0);
     uint c = a / b;
     assert(a == b * c + a % b);
     return c;
   }
 
-  function safeSub(uint a, uint b) internal returns (uint) {
+  function safeSub(uint a, uint b) internal pure returns (uint) {
     assert(b <= a);
     return a - b;
   }
 
-  function safeAdd(uint a, uint b) internal returns (uint) {
+  function safeAdd(uint a, uint b) internal pure returns (uint) {
     uint c = a + b;
     assert(c>=a && c>=b);
     return c;
   }
 
-  function max(uint a, uint b) internal constant returns (uint) {
+  function max(uint a, uint b) internal pure returns (uint) {
     return a >= b ? a : b;
   }
 
-  function min(uint a, uint b) internal constant returns (uint) {
+  function min(uint a, uint b) internal pure returns (uint) {
     return a < b ? a : b;
   }
 }
