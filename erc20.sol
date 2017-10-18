19fabc6b9f46a4ce194ef4443f403124320951c9
JonnyLatte <johnlaloli@gmail.com>
2017-10-19 01:15:49 +1100

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.12;
 contract ERC20 {
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval( address indexed owner, address indexed spender, uint value);
-    function totalSupply() constant returns (uint supply);
-    function balanceOf( address who ) constant returns (uint value);
-    function allowance(address owner, address spender) constant returns (uint _allowance);   
-    function transfer( address to, uint value) returns (bool ok);
-    function transferFrom( address from, address to, uint value) returns (bool ok);
-    function approve(address spender, uint value) returns (bool ok);
+    function totalSupply() public constant returns (uint supply);
+    function balanceOf( address who ) public constant returns (uint value);
+    function allowance(address owner, address spender) public constant returns (uint _allowance);   
+    function transfer( address to, uint value) public returns (bool success);
+    function transferFrom( address from, address to, uint value) public  returns (bool success);
+    function approve(address spender, uint value) public returns (bool success);
 }
