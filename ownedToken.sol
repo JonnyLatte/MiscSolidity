26b4861d8f8a19930f26e325a0ad4ec1ff776901
JonnyLatte <johnlaloli@gmail.com>
2017-10-19 11:11:42 +1100

    Update ownedToken.sol

--- ownedToken.sol
+++ ownedToken.sol
@@ -5,25 +5,25 @@ import "github.com/JonnyLatte/MiscSolidity/owned.sol";
 
 contract ownedToken is baseToken, owned  {
     
-    function mint(address to, uint256 value) onlyOwner  returns (bool ok)
+    function mint(address to, uint256 value) public onlyOwner  returns (bool success)
     {
         _balances[to] = _balances[to].safeAdd(value);
         _supply = _supply.safeAdd(value);
         return true;
     }
     
-    function burn(address from,uint256 value) onlyOwner  returns (bool ok)
+    function burn(address from,uint256 value) public onlyOwner  returns (bool success)
     {
         _balances[from] = _balances[from].safeSub(value);
         _supply = _supply.safeSub(value);
         return true;
     }
     
-    function destroy() onlyOwner {
-        suicide(msg.sender);
+    function destroy() public onlyOwner {
+        selfdestruct(msg.sender);
     }
  
-    function () {
-        throw;
+    function () public {
+        revert();
     }
 }
