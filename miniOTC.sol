566c3a4b19b82174ada19609774cc159185f1450
JonnyLatte <johnlaloli@gmail.com>
2017-11-04 23:55:49 +1100

    Update miniOTC.sol

--- miniOTC.sol
+++ miniOTC.sol
@@ -14,7 +14,7 @@ contract miniOTC {
 
     event onTrade(uint256 unitLots);
     
-    function  miniOTC(ERC20 _maker_buy_token, uint256 _maker_buy_units, ERC20 _maker_sell_token, uint256 _maker_sell_units) 
+    function  miniOTC(ERC20 _maker_buy_token, uint256 _maker_buy_units, ERC20 _maker_sell_token, uint256 _maker_sell_units) public
     {
        maker_buy_token    = _maker_buy_token;
        maker_buy_units    = _maker_buy_units; 
@@ -22,7 +22,7 @@ contract miniOTC {
        maker_sell_units   = _maker_sell_units; 
     }
 
-    function trade(address taker, address maker, uint256 unitLots)
+    function trade(address taker, address maker, uint256 unitLots) public
     {
         uint takerValue = unitLots.safeMul(maker_buy_units);
         uint makerValue = unitLots.safeMul(maker_sell_units);
@@ -36,7 +36,7 @@ contract miniOTCFactory {
     
     event tradeListing(address contractAddress, ERC20 indexed _maker_buy_token, uint256 _maker_buy_units, ERC20 indexed _maker_sell_token, uint256 _maker_sell_units);
     
-    function  createTrade(ERC20 _maker_buy_token, uint256 _maker_buy_units, ERC20 _maker_sell_token, uint256 _maker_sell_units) 
+    function  createTrade(ERC20 _maker_buy_token, uint256 _maker_buy_units, ERC20 _maker_sell_token, uint256 _maker_sell_units) public
     {
         tradeListing(
             new miniOTC(_maker_buy_token, _maker_buy_units, _maker_sell_token, _maker_sell_units) , 
