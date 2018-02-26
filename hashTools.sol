4fcac6539139ce0e4b156b44362e77ca1cdfb1db
JonnyLatte <johnlaloli@gmail.com>
2018-02-26 20:37:23 +1100

    Update hashTools.sol

--- hashTools.sol
+++ hashTools.sol
@@ -15,5 +15,25 @@ library hashTools {
             return(0x0, 32)
         }
     }
-}
+    
+    
+    // recover address from signature data
+    function checkSig(bytes32 hash, bytes sig) internal pure returns(address) 
+    {
+        //https://gitter.im/ethereum/solidity/archives/2016/12/14
+        
+        uint8   v;
+        bytes32 r;
+        bytes32 s;
 
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := and(mload(add(sig, 65)), 255)
+        }
+    
+        if (v < 27) v += 27;
+    
+        return ecrecover(hash, v, r, s) ;
+    } 
+}
