726ea0e719c416dfa1aa6deb88d32647bdaab576
JonnyLatte <johnlaloli@gmail.com>
2018-02-26 20:35:56 +1100

    Update verySig.sol

--- verySig.sol
+++ verySig.sol
@@ -1,7 +1,23 @@
 pragma solidity ^0.4.12;
 
-library verySig {
+
+library hashTools {
+  
+    // reverse the bytes of a byte32 value
+    function reverseBytes32(bytes32 x) internal pure returns (bytes32) {
+        for(uint i = 0; i < 32; i++) {        
+            assembly {
+                mstore8(i,x) 
+                x := div(x,256)            
+            }
+        }
+        assembly {
+            return(0x0, 32)
+        }
+    }
     
+    
+    // recover address from signature data
     function checkSig(bytes32 hash, bytes sig) internal pure returns(address) 
     {
         //https://gitter.im/ethereum/solidity/archives/2016/12/14
@@ -19,5 +35,5 @@ library verySig {
         if (v < 27) v += 27;
     
         return ecrecover(hash, v, r, s) ;
-    }    
+    } 
 }
